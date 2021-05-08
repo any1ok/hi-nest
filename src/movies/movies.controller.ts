928ccdd2d37ec2429b0f82314f8e1315ec58421b
any1ok <any3ok@naver.com>
2021-05-08 23:58:55 +0900

    change

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -8,14 +8,23 @@ export class MoviesController {
         return 'This will return all movies';
     }
 
+
+    @Get("search")
+    search(@Query("year") movieYear) {
+        console.log(movieYear);
+        return `We are searching for a movie with a title : ${movieYear}`
+    }
+
     @Get("/:id")
     getOne(@Param("id") id: string) {
         return `This will return one movies with the id: ${id}`;
     }
 
     @Post()
-    create() {
-        return 'this will create a movie';
+    create(@Body() movieData) {
+        console.log(movieData);
+        return movieData;
+        //return 'this will create a movie';
     }
 
     @Delete('/:id')
@@ -24,7 +33,13 @@ export class MoviesController {
     }
 
     @Patch('/:id')
-    patch(@Param("id") movieId: string) {
-        return `this will delete a movie with the id: ${movieId}`
+    patch(@Param("id") movieId: string, @Body() updateData) {
+        return {
+            updateMovie: movieId,
+            ...updateData
+        }
+        //return `this will delete a movie with the id: ${movieId}`
     }
+
+
 }
