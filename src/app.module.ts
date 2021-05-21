278059a4a414ebeedf09cf5a1c039d1f67d7a2cd
any1ok <any3ok@naver.com>
2021-05-21 17:04:30 +0900

    food

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
 import { FoodController } from './food/food.controller';
+import { FoodService } from './food/food.service';
+import { FoodModule } from './food/food.module';
 
 @Module({
-  imports: [],
+  imports: [FoodModule],
   controllers: [AppController, MoviesController, FoodController],
-  providers: [AppService, MoviesService],
+  providers: [AppService, MoviesService, FoodService],
 })
 export class AppModule {}
