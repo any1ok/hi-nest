7bd44368b50865c975d4cdaa7da79073df8102d8
any1ok <any3ok@naver.com>
2021-05-26 15:17:01 +0900

    dd

--- src/food/food.controller.ts
+++ src/food/food.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
-
+import { Body, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 @Controller('food')
-export class FoodController {}
+export class FoodController {
+
+    @Get("search")
+    search(@Query("year") movieYear) {
+        console.log(movieYear);
+        return `We are searching for a movie with a title : ${movieYear}`
+    }
+
+}
